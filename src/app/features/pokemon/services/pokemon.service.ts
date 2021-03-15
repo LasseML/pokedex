ebb4a573f718d3badfcaae6057621cc011b290b9
Lasse Minet <78145641+LasseML@users.noreply.github.com>
2021-03-15 14:57:10 +0100

    feat: added pokemon api connection

---
+++ src/app/features/pokemon/services/pokemon.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../../../environments/environment';
+import {map, shareReplay} from 'rxjs/operators';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Pokemon} from '../../../models/pokemon.model';
+import {PokemonResponse} from '../../../models/pokemon-response.model';
+
+const { pokeAPI } = environment;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  // @ts-ignore
+  private readonly pokemonCache$;
+  public pokemon: Pokemon[] = [];
+  public error: string = '';
+
+  constructor(private readonly http: HttpClient) {
+    this.pokemonCache$ = this.http.get<PokemonResponse>(`${pokeAPI}/pokemon`)
+      .pipe(shareReplay(1));
+  }
+
+  fetchPokemon(): void {
+    this.pokemonCache$
+      .pipe(
+        map((response: PokemonResponse) => {
+          // @ts-ignore
+          return response.results.map((pokemon: Pokemon) => ({
+            ...pokemon,
+            ...this.getIdAndImage(pokemon.url)
+          }));
+        })
+      ).subscribe(
+          (pokemon: Pokemon[]) => {
+          this.pokemon = pokemon;
+        },
+          (errorResponse: HttpErrorResponse) => {
+          this.error = errorResponse.message;
+        }
+    );
+  }
+
+  private getIdAndImage(url: string): any {
+    const id = url.split('/').filter(Boolean).pop();
+    return {
+      id: Number(id),
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+    };
+  }
+}
