31dc76ca59332beaec198edfa4e76ce8921ffeb7
Lasse Minet <78145641+LasseML@users.noreply.github.com>
2021-03-16 17:49:22 +0100

    fix: fixed getting storage on trainer page

--- src/app/features/trainer/containers/trainer-container.component.ts
+++ src/app/features/trainer/containers/trainer-container.component.ts
@@ -1,19 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {Pokemon} from '../../../models/pokemon.model';
+import {getStorage} from '../../../utils/storage.utils';
 
 @Component({
   selector: 'app-trainer-container',
   templateUrl: './trainer-container.component.html'
 })
 export class TrainerContainerComponent implements OnInit {
-
-  constructor() {
-  }
-
   get pokemon(): Pokemon[] {
-    return [];
+    const existingCollection: any = getStorage('collection');
+    return existingCollection;
   }
-
 
   ngOnInit(): void {
     return;
